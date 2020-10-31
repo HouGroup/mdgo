4f3da141b1e251def5d3b6c1864e2f03df03bf89
Tingzheng Hou <tingzheng_hou@berkeley.edu>
2020-10-30 18:14:02 -0700

    hopping analysis (w/ self hopping)

--- mdgo/coordination.py
+++ mdgo/coordination.py
@@ -1,5 +1,89 @@
 import numpy as np
 from tqdm import tqdm_notebook
+from MDAnalysis.analysis.distances import distance_array
+from scipy.signal import savgol_filter
+from itertools import groupby
+
+
+def trajectory(nvt_run, li_atom, run_start, run_end, species, selection_dict, distance):
+    A_values = {}
+    time_count = 0
+    trj_analysis = nvt_run.trajectory[run_start:run_end:]
+    if species not in list(selection_dict):
+        print('Invalid species selection')
+        return None
+    for ts in trj_analysis:
+        selection = "(" + selection_dict[species] + ") and (around " \
+                    + str(distance) + " index " \
+                    + str(li_atom.id - 1) + ")"
+        shell = nvt_run.select_atoms(selection, periodic=True)
+        for atom in shell.atoms:
+            if str(atom.id) not in A_values:
+                A_values[str(atom.id)] = np.full(run_end-run_start, 100.)
+        time_count += 1
+    #print(A_values.keys())
+    time_count = 0
+    for ts in trj_analysis:
+        for atomid in A_values.keys():
+            dist = distance_array(ts[li_atom.id-1], ts[(int(atomid)-1)], ts.dimensions)
+            A_values[atomid][time_count] = dist
+        time_count += 1
+    return A_values
+
+
+def find_nearest(trj, time_step, distance, hopping_cutoff, smooth=51):
+    time_span = len(list(trj.values())[0])
+    for kw in list(trj):
+        trj[kw] = savgol_filter(trj.get(kw), smooth, 2)
+    site_distance = [100 for _ in range(time_span)]
+    sites = [0 for _ in range(time_span)]
+    sites[0] = min(trj, key=lambda k: trj[k][0])
+    site_distance[0] = trj.get(sites[0])[0]
+    for time in range(1, time_span):
+        if sites[time - 1] == 0:
+            old_site_distance = 100
+        else:
+            old_site_distance = trj.get(sites[time - 1])[time]
+        if old_site_distance > hopping_cutoff:
+            new_site = min(trj, key=lambda k: trj[k][time])
+            new_site_distance = trj.get(new_site)[time]
+            if new_site_distance > distance:
+                site_distance[time] = 100
+            else:
+                sites[time] = new_site
+                site_distance[time] = new_site_distance
+        else:
+            sites[time] = sites[time-1]
+            site_distance[time] = old_site_distance
+    sites = [int(i) for i in sites]
+    sites_and_distance_array = np.array([[sites[i], site_distance[i]]
+                                         for i in range(len(sites))])
+    steps = []
+    closest_step = 0
+    previous_site = sites_and_distance_array[0][0]
+    for i, step in enumerate(sites_and_distance_array):
+        site = step[0]
+        distance = step[1]
+        if site == 0:
+            previous_site = None
+        else:
+            if site == previous_site:
+                if distance < sites_and_distance_array[closest_step][1]:
+                    closest_step = i
+                else:
+                    pass
+            else:
+                steps.append(closest_step)
+                closest_step = i
+                previous_site = site
+    if previous_site is not None:
+        steps.append(closest_step)
+
+    grouped_sites = [list(v) for k, v in groupby(sites, key=lambda x: x != 0)
+                     if k != 0]
+    change = (np.diff(sites) != 0).sum() - len(grouped_sites) + 1
+    frequency = change/(time_span * time_step)
+    return sites, frequency, steps
 
 
 def num_of_neighbor_one_li(nvt_run, li_atom, species, select_dict,
--- mdgo/core.py
+++ mdgo/core.py
@@ -5,13 +5,15 @@ from MDAnalysis.analysis.rdf import InterRDF
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from MDAnalysis.analysis.distances import distance_array
 import re
 from statsmodels.tsa.stattools import acovf
 from scipy.optimize import curve_fit
 from tqdm import tqdm_notebook
 from mdgo.conductivity import calc_cond, conductivity_calculator
-from mdgo.coordination import coord_shell_array, num_of_neighbor_one_li
-from mdgo.msd import total_msd, partial_msd, partial_msd_new
+from mdgo.coordination import\
+    coord_shell_array, num_of_neighbor_one_li, trajectory, find_nearest
+from mdgo.msd import total_msd, partial_msd, special_msd
 from mdgo.residence_time import calc_neigh_corr, fit_residence_time
 
 
@@ -50,7 +52,7 @@ class MdRun:
         gas_constant = 8.314
         temp = 298.15
         faraday_constant_2 = 96485 * 96485
-        self.c = (self.num_li / (self.nvt_v * (1e-30))) / (6.022*1e23)
+        self.c = (self.num_li / (self.nvt_v * 1e-30)) / (6.022*1e23)
         self.d_to_sigma = self.c * faraday_constant_2 / (gas_constant * temp)
 
     def get_init_dimension(self):
@@ -126,27 +128,23 @@ class MdRun:
                               select=self.select_dict["cation"], fft=fft)
         return msd_array
 
+    def get_msd_by_length(self, distance, run_start, run_end):
+        nvt_run = self.unwrapped_run
+        li_atoms = nvt_run.select_atoms(self.select_dict["cation"])
+        free_array, attach_array = special_msd(nvt_run, li_atoms,
+                                               self.select_dict, distance,
+                                               run_start, run_end)
+        return free_array, attach_array
+
     def get_msd_partial(self, distance, run_start, run_end, largest=1000):
         nvt_run = self.unwrapped_run
         li_atoms = nvt_run.select_atoms(self.select_dict["cation"])
         free_array, attach_array = partial_msd(nvt_run, li_atoms, largest,
                                                self.select_dict, distance,
                                                run_start, run_end)
-        free_array.columns = ['msd']
-        attach_array.columns = ['msd']
-        return free_array, attach_array
-
-    def get_msd_partial_new(self, distance, run_start, run_end, largest=1000):
-        nvt_run = self.unwrapped_run
-        li_atoms = nvt_run.select_atoms(self.select_dict["cation"])
-        free_array, attach_array = partial_msd_new(nvt_run, li_atoms, largest,
-                                                   self.select_dict, distance,
-                                                   run_start, run_end)
         return free_array, attach_array
 
     def get_d(self, msd_array, start, stop, percentage=1):
-        if isinstance(msd_array, pd.DataFrame):
-            msd_array = msd_array["msd"].to_numpy()
         a2 = 1e-20
         ps = 1e-12
         s_m_to_ms_cm = 10
@@ -154,14 +152,14 @@ class MdRun:
             d = (msd_array[start] - msd_array[stop]) \
                 / (start-stop) / self.time_step / 6 * a2 / ps
             sigma = percentage * d * self.d_to_sigma * s_m_to_ms_cm
-            print("Diffusivity of ", percentage*100, "% Li: ", d, "m^2/s")
-            print("Conductivity of ", percentage*100, "% Li: ", sigma, "mS/cm")
+            print("Diffusivity of", "%.2f" % (percentage * 100) + "% Li: ", d, "m^2/s")
+            print("Conductivity of", "%.2f" % (percentage * 100) + "% Li: ", sigma, "mS/cm")
         else:
             d = (msd_array[start] - msd_array[stop]) \
                 / (start - stop) / self.time_step / 6 * a2 / ps
             sigma = d * self.d_to_sigma * s_m_to_ms_cm
-            print("Diffusivity of all Li: ", d, "m^2/s")
-            print("Conductivity of all Li: ", sigma, "mS/cm")
+            print("Diffusivity of all Li:", d, "m^2/s")
+            print("Conductivity of all Li:", sigma, "mS/cm")
 
     def get_neighbor_corr(self, species_list, distance, run_start, run_end):
         return calc_neigh_corr(self.wrapped_run, species_list, self.select_dict,
@@ -169,4 +167,39 @@ class MdRun:
 
     @staticmethod
     def get_residence_time(species_list, times, acf_avg_dict, cutoff_time):
-        return fit_residence_time(times, species_list, acf_avg_dict, cutoff_time)
+        return fit_residence_time(times, species_list,
+                                  acf_avg_dict, cutoff_time)
+
+    def get_neighbor_trj(self, run_start, run_end, li_atom, species, distance):
+        return trajectory(self.wrapped_run, li_atom, run_start, run_end, species,
+                          self.select_dict, distance)
+
+    def get_hopping_freq_dist(self, run_start, run_end, species, distance,
+                              hopping_cutoff, smooth=51):
+        nvt_run = self.wrapped_run
+        li_atoms = nvt_run.select_atoms(self.select_dict["cation"])
+        freqs = []
+        hopping_distance = []
+        for li in tqdm_notebook(li_atoms[:]):
+            neighbor_trj = trajectory(nvt_run, li, run_start, run_end, species,
+                                      self.select_dict, distance)
+            sites, freq, steps = find_nearest(neighbor_trj, self.time_step,
+                                              distance, hopping_cutoff,
+                                              smooth=smooth)
+
+            coords = []
+            for step in steps:
+                coord_li = nvt_run.trajectory[step + 1000][li.id - 1]
+                coords.append(coord_li)
+            if len(coords) > 1:
+                dists = []
+                for i in range(len(coords) - 1):
+                    dist = distance_array(coords[i + 1], coords[i],
+                                          box=self.get_nvt_dimension())[0][0]
+                    dists.append(dist)
+                    print(dist)
+                li_mean_dists = np.mean(dists)
+                hopping_distance.append(li_mean_dists)
+            freqs.append(freq)
+        return np.mean(freqs), np.mean(hopping_distance)
+
