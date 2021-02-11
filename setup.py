e21defd3dfdea4acb693ab83a5ae4186d16bbb2e
Tingzheng Hou <tingzheng_hou@berkeley.edu>
2021-02-11 13:27:32 -0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,29 @@ from setuptools import setup, find_packages
 
 module_dir = os.path.dirname(os.path.abspath(__file__))
 
+with open(os.path.join(module_dir, "README.md"), 'r') as f:
+    readme = f.read()
+
 if __name__ == "__main__":
     setup(
         name='mdgo',
-        version='2021.1.15',
+        version='2021.2.11',
+        description='A codebase for MD simulation setup and results analysis.',
+        long_description=readme,
+        long_description_content_type="text/markdown",
+        license="MIT",
+        author="mdgo development team",
+        author_email='tingzheng_hou@berkeley.edu',
+        maintainer="Tingzheng Hou",
+        maintainer_email='tingzheng_hou@berkeley.edu',
+        url="https://github.com/htz1992213/mdgo",
         packages=find_packages(),
-        install_requires=["tqdm", "pymatgen", "matplotlib", "statsmodels", "re",
-                          "MDAnalysis==2.0.0-dev0", "pandas", "numpy", "scipy"],
-        description='Repository for analyzing MD results',
+        install_requires=[
+            "tqdm",
+            "pymatgen",
+            "statsmodels",
+            "MDAnalysis==2.0.0.dev0",
+            "selenium"],
         python_requires='>=3.6'
     )
+
