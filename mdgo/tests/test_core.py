cedb07c4c46b45251a83d242388068e0714b1c71
orioncohen <orion@lbl.gov>
2021-04-06 15:05:45 -0700

    updated core.py testing

--- mdgo/tests/test_core.py
+++ mdgo/tests/test_core.py
@@ -2,6 +2,7 @@ import os
 import pathlib
 import unittest
 from mdgo.core import MdRun
+import numpy as np
 
 
 class MdRunTest(unittest.TestCase):
@@ -13,33 +14,35 @@ class MdRunTest(unittest.TestCase):
         cls.unwrapped_dcd = gen2_dir / "data_unwrapped_nvt.dcd"
         select_dict = {"cation": "type 22", "anion": "type 20"}
         cls.md_run = MdRun(cls.data_file, cls.unwrapped_dcd,
-                        500, 1000, "BN_FEC_LiPF6", select_dict)
-
+                           500, 10, "BN_FEC_LiPF6", select_dict)
         cls.res_mass_dict = {'Li': 6.94, 'PF6': 144.97,
                              'FEC': 106.05, 'BN': 69.105}
         cls.md_autorun = \
             MdRun.auto_constructor(cls.data_file, cls.unwrapped_dcd,
-                                   500, 1000, "BN_FEC_LiPF6", cls.res_mass_dict,
+                                   500, 10, "BN_FEC_LiPF6", cls.res_mass_dict,
                                    "Li", "PF6", "P", ["BN", "FEC"])
 
     def test_normal_constructor(self):
         run = self.md_run
-        print(run.select_dict)
-        print(run.unwrapped_run.atoms.types)
         self.assertEqual(48, len(run.cations))
         self.assertEqual(48, len(run.anion_center))
         self.assertEqual(336, len(run.anions))
         self.assertTrue(set(['anion', 'cation']) ==
                         set(run.select_dict.keys()))
+        print(self.md_run.time_array)
+        print(self.md_run.cond_array)
+        print(run.get_conductivity())
 
     def test_auto_constructor(self):
         autorun = self.md_autorun
+        run = self.md_run
         self.assertEqual(48, len(autorun.cations))
         self.assertEqual(48, len(autorun.anion_center))
         self.assertEqual(336, len(autorun.anions))
         self.assertEqual(2, len(autorun.electrolytes))
         self.assertTrue(set(['anion', 'cation', 'BN', 'FEC']) ==
                         set(autorun.select_dict.keys()))
+        np.testing.assert_array_almost_equal(run.cond_array, autorun.cond_array)
 
     def test_get_ion_pairing(self):
         # this function only works with the autorun constructor
@@ -49,4 +52,3 @@ class MdRunTest(unittest.TestCase):
         print(raw_counts)
         self.assertEqual(34, raw_counts['SSIP'])
         self.assertEqual(14, raw_counts['CIP'])
-
