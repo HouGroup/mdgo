0430e72e94107402b82e8a1e7970e3118573c0ed
Tingzheng Hou <tingzheng_hou@berkeley.edu>
2021-04-13 00:17:22 -0700

    reconfig logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,7 +61,11 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "_static/logo_mdgo.png"
+html_logo = "_static/logo_mdgo.svg"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 autodoc_mock_imports = [
     "numpy",
