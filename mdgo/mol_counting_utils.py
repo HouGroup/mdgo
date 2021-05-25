2c6d7d4e7d8b0026140810a45ff8493e2990e5b0
orioncohen <orion@lbl.gov>
2021-05-25 13:46:51 -0700

    changed counting_utils.py to mol_counting_utils.py

---
+++ mdgo/mol_counting_utils.py
@@ -0,0 +1,30 @@
+# atom group counters
+from mdgo.shell_functions import get_cation_anion_shells
+import numpy as np
+
+"""
+This file exists as a placeholder, it should eventually hold the counting utilities
+that are currently implemented in shell_functions.
+
+However for clarity of pull requests, I chose to temporarily implement those functions
+in shell functions.
+
+This will implement get_counts, get_pair_type, count_dicts
+"""
+
+# def get_detailed_cation_speciation(self, timestep):
+#     """
+#     This function should be move to a utility folder and wrapped, right now
+#     its not very useful. Currently broken
+#
+#     Returns:
+#
+#     """
+#     self.u_wrapped.trajectory[timestep]
+#     solvation_shell_speciation = [get_counts(get_radial_shell(self.u, cation, 3))
+#                                   for cation in self.cations]
+#     counts_by_speciation = count_dicts(solvation_shell_speciation)
+#     print('before: ', len(solvation_shell_speciation))
+#     print('after: ', len(counts_by_speciation))
+#     print('after: ', sum(counts_by_speciation.keys()))
+#     return counts_by_speciation
